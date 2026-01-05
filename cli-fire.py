697bb077d9415612c053486356e589b7913ded0e
dantepilla75 <pilladanteswararao@gmail.com>
2026-01-05 22:07:37 +0000

    New cli functionality is included

--- cli-fire.py
+++ cli-fire.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import fire
-from mylib.logic import wiki
+from mylib.logic import wiki, search_wiki
 
 if __name__ == "__main__":
-    fire.Fire(wiki)
+    fire.Fire()
--- mylib/logic.py
+++ mylib/logic.py
@@ -6,3 +6,9 @@ def wiki(name="War Goddess", length=1):
 
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+def search_wiki(name):
+    """Search Wikipedia for Names"""
+
+    search_results = wikipedia.search(name)
+    return search_results
